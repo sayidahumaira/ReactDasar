2f9320f69c4d07760e71b9577f495e8642d5ae03
sayidahumaira <sayidabts@gmail.com>
2023-02-08 02:03:50 +0700

    jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import Gallery from './components/Gallery.js';
+// import Gallery from './components/Gallery.js';
+import Todolist from './components/Todolist.js';
 
 export default function App() {
   return (
-    <Gallery />
+    <Todolist />
   );
 }
 
