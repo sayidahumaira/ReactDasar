a65889d93ee32a1eab0dc6710d813634e4bb94ab
sayidahumaira <sayidabts@gmail.com>
2023-02-12 22:34:38 +0700

    button simple

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
 import React from 'react';
-import './App.css';
-
-function Gelas(){
-     let cups = [];
-     
-    for (let i = 1; i <= 12; i++) {
-    cups.push(<Cup key={i} guest={i} />);
-  }
-  return cups;
-}
-
-function Cup({ guest }) {
-  return <h2>Tea cup for guest #{guest}</h2>;
-}
-
-function Recipe({ drinkers }) {
-  return (
-    <ol>    
-      <li>Boil {drinkers} cups of water.</li>
-      <li>Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.</li>
-      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
-    </ol>
-  );
-}
 
 export default function App() {
   return (
-    <section>
-      <h1>Spiced Chai Recipe</h1>
-      <h2>For two</h2>
-      <Recipe drinkers={2} />
-      <h2>For a gathering</h2>
-      <Recipe drinkers={4} />
-      <Cup guest={1} />
-      <Cup guest={2} />
-      <Cup guest={3} />
-      <h4>Testing Pure Gelas</h4>
-      <Gelas />
-    </section>
+    <TombolSimple
+      onPlayMovie={() => alert('Playing!')}
+      onUploadImage={() => alert('Uploading!')}
+    />
+  );
+}
+
+function TombolSimple({ onPlayMovie, onUploadImage }) {
+  return (
+    <div>
+      <Button onClick={onPlayMovie}>
+        Play Movie
+      </Button>
+      <Button onClick={onUploadImage}>
+        Upload Image
+      </Button>
+    </div>
+  );
+}
+
+function Button({ onClick, children}) {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
   );
 }
