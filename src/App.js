d6a758352c32ac03e3a405970d9b6ba8f990860f
sayidahumaira <sayidabts@gmail.com>
2023-02-13 22:24:41 +0700

    9.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function App() {
-  const [to, setTo] = useState('Ayana');
-  const [pesan, setPesan] = useState('Hello');
+  const [score, setScore] = useState(0);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTimeout(() => {
-      alert(`You said ${pesan} to ${to}`);
-    }, 1500);
+    //fungsi nya
+  function increment() {
+    setScore(s => s + 1);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        To:{' '}
-        <select
-          value={to}
-          onChange={e => setTo(e.target.value)}>
-          <option value="Ayana">Ayana</option>
-          <option value="Balqis">Balqis</option>
-        </select>
-      </label>
-      <textarea
-        placeholder="Message"
-        value={pesan}
-        onChange={e => setPesan(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
-  );
+    <>
+      <button onClick={() => increment()}>+1</button>
+      <button onClick={() => {
+        increment();
+        increment();
+        increment();
+      }}>+3</button>
+      <h1>Score: {score}</h1>
+    </>
+  )
 }
