ef55535c7e08d746ba8d7d38f23d472a198f70f6
sayidahumaira <sayidabts@gmail.com>
2023-02-14 20:14:03 +0700

    red dot pointer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import { useState } from 'react';
-
 export default function App() {
-  const [score, setScore] = useState(0);
-
-    //fungsi nya
-  function increment() {
-    setScore(s => s + 1);
-  }
-
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+  });
   return (
-    <>
-      <button onClick={() => increment()}>+1</button>
-      <button onClick={() => {
-        increment();
-        increment();
-        increment();
-      }}>+3</button>
-      <h1>Score: {score}</h1>
-    </>
-  )
+    <div
+      onPointerMove={e => {
+        setPosition({
+          x: e.clientX,
+          y: e.clientY
+        });
+      }}
+      style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+      }}>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        borderRadius: '50%',
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        left: -10,
+        top: -10,
+        width: 20,
+        height: 20,
+      }} />
+    </div>
+  );
 }
